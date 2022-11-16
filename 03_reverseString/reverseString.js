943dfaf75906104882e9afc851bb835325502c05
insertSoul <j.clarke213@gmail.com>
2022-11-16 18:03:50 +0000

    Complete reverse string passing all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray = string.split("");
+    stringArray = stringArray.reverse();
+    let reverseString = stringArray.join("");
+    return reverseString;
 };
+
+reverseString("hello there")
 
 // Do not edit below this line
 module.exports = reverseString;

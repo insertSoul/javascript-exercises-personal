0fa170abac39815708773df44621875923fc5c21
insertSoul <j.clarke213@gmail.com>
2022-11-16 16:33:59 +0000

    Complete exersise 02, passing all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    if (number >= 0) {
+        return string.repeat(number);
+    } else {
+        return ("ERROR");
+    }
 };
+number = Math.floor(Math.random() * 1000);
+repeatString('', number);
 
 // Do not edit below this line
 module.exports = repeatString;

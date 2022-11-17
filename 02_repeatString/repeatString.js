885f729f10b09ae6350b700e3d8d178f2a897493
insertSoul <j.clarke213@gmail.com>
2022-11-17 22:21:47 +0000

    Update function so it uses a loop

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,3 +1,17 @@
+const repeatString = function(string, repeats) {
+    if (repeats < 0) {
+        return "ERROR";
+    } 
+    let theRepeatedString = "";
+    for (let i=0 ; i < repeats ; i++) {
+        theRepeatedString += string;
+    }
+    return theRepeatedString;
+}
+
+
+// Old method below did not use a loop
+/*
 const repeatString = function(string, number) {
     if (number >= 0) {
         return string.repeat(number);
@@ -7,6 +21,6 @@ const repeatString = function(string, number) {
 };
 number = Math.floor(Math.random() * 1000);
 repeatString('', number);
-
+*/
 // Do not edit below this line
 module.exports = repeatString;

524976c104f2afd8aab1da26145758df248c62c9
insertSoul <j.clarke213@gmail.com>
2022-11-18 11:23:58 +0000

    Built basic function for removing items

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,25 @@
-const removeFromArray = function() {
+
+let discardValue = []
+const removeFromArray = function( array, itemToRemove ) {
+   for (let i = 0 ; i <= array.length -1 ; i++) {
+       if (array[i] == itemToRemove) {
+         discardValue = array.splice(i, 1);
+       }
+   }
+   console.log(array);
+   return array;
 
 };
 
+
+
+
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+// Loop through the array
+// check if items are equal
+// if equal remove at givin index
+
+// Change funtion to accept multiple arguments

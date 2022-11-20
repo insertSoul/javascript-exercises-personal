dbe696c276236f35e19980fab4bd6151e104fe22
insertSoul <j.clarke213@gmail.com>
2022-11-20 21:37:30 +0000

    Add a function in to sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    if (int2 < int1) {
+        [int2, int1] = [int1, int2]
+    }
+    let totalSum = 0
+    for (let i = int1; i <= int2; i++) {
+        totalSum += i
+    }
+    return totalSum
 
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+// take 2 intergers
+// loop increaseing
+//start at int1, stop at (and include) int2
+// on each loop add the values together
+// return final addition

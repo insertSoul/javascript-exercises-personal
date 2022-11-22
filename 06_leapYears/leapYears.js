6300b789b1fa751bb01ee0be6ba834211f4a06f7
insertSoul <j.clarke213@gmail.com>
2022-11-22 22:53:54 +0000

    Create a function to detrmine if year is a leapYear

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,24 @@
-const leapYears = function() {
+/*
+const leapYears = function(theYear) {
+    if (theYear % 400 == 0) return true;
+    if (theYear % 4 == 0 && theYear % 100 != 0) {
+        return true;
+    } else return false;
+
+};
+*/
+
+// refactor into one line
+const leapYears = function(theYear) {
+    if (theYear % 400 == 0 || (theYear % 4 == 0 && theYear % 100 != 0)) {
+        return true;
+    } else return false;
 
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+
+// if divisible by 4 and not divisiable by 100 it is a leap year 
+// unless divisable by 400!

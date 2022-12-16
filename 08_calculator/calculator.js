36d34987f44cb25a2fe38f4a8f35eab800b52830
insertSoul <j.clarke213@gmail.com>
2022-12-16 17:44:02 +0000

    Complete first three functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,13 +1,20 @@
-const add = function() {
+const add = function(num1, num2) {
+  return num1 + num2;
 	
 };
 
-const subtract = function() {
+const subtract = function(num1, num2) {
+  return num1 - num2;
 	
 };
 
-const sum = function() {
-	
+const sum = function(inputArray) {
+	initiaValue = 0;
+  const sumOfArray = inputArray.reduce(
+    (total, currentValue) => total + currentValue, initiaValue 
+  );
+  return sumOfArray;
+
 };
 
 const multiply = function() {

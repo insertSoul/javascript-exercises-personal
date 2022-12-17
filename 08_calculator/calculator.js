cfe990921e5f3cfc2cfaff2eff4d3f7636c09d1a
insertSoul <j.clarke213@gmail.com>
2022-12-17 22:08:49 +0000

    Complete all calc functions and tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,39 @@
 const add = function(num1, num2) {
   return num1 + num2;
-	
 };
 
 const subtract = function(num1, num2) {
   return num1 - num2;
-	
 };
 
 const sum = function(inputArray) {
-	initiaValue = 0;
   const sumOfArray = inputArray.reduce(
-    (total, currentValue) => total + currentValue, initiaValue 
+    (total, currentValue) => total + currentValue, 0
   );
   return sumOfArray;
-
 };
 
-const multiply = function() {
-
+const multiply = function(inputArray) {
+  const productOfArray = inputArray.reduce((total, currentValue) => total * currentValue , 1
+  );
+  return productOfArray;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return num1 ** num2
 };
 
-const factorial = function() {
-	
-};
+
+const factorial = function(num) {
+  if (num == 0 || num == 1) {
+    return 1;
+  }
+  for (let i = num -1 ; i >= 1; i--) {
+    num *= i;
+  }
+  return num;
+  };
+
 
 // Do not edit below this line
 module.exports = {

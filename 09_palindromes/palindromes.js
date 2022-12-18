3cadcdd60188d9dd062f234824f12352d58489d2
insertSoul <j.clarke213@gmail.com>
2022-12-18 17:35:51 +0000

    Pass all tests for plaindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (startingString) {
+    newString = startingString.toLowerCase()
+    newString = newString.replace(/\W/g, ''); //Remove non-alphanumerics
+    newArray = newString.split('');
+    midpoint = ((newArray.length -1)/2) + 1
+    for(let i = 0 ; i < midpoint ; i++) {
+        if (newArray[i] == newArray[(newArray.length -i -1 )]) {
+        } else {
+            return false;
+        }
+    } return true;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;

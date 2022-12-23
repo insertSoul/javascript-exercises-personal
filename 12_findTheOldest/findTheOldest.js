a8d25eff35b630b048e1cbc80f10a861c25df755
insertSoul <j.clarke213@gmail.com>
2022-12-23 10:18:21 +0000

    Update function to work if person is still living

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,29 +1,17 @@
+function totalYearsLived(maxAge,yearOfBirth) {
+    if (!maxAge) {
+        maxAge = new Date().getFullYear();
+    }
+    return maxAge - yearOfBirth;
+};
+
 const findTheOldest = function(array) {
    return array.reduce((oldest, currentPerson) => {
-       const oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
-       const currentPersonAge = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
+       const oldestAge = totalYearsLived(oldest.yearOfDeath, oldest.yearOfBirth);
+       const currentPersonAge = totalYearsLived(currentPerson.yearOfDeath, currentPerson.yearOfBirth);
        return oldestAge > currentPersonAge ? oldest : currentPerson ;
 });
 };
 
-/*
-const people = [
-    {
-      name: "Carly",
-      yearOfBirth: 1942,
-      yearOfDeath: 1970,
-    },
-    {
-      name: "Ray",
-      yearOfBirth: 1962,
-      yearOfDeath: 2011,
-    },
-    {
-      name: "Jane",
-      yearOfBirth: 1912,
-      yearOfDeath: 1941,
-    },
-  ]
-*/
 // Do not edit below this line
 module.exports = findTheOldest;

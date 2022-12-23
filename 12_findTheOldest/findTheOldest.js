59352279d44213b2450d8a3424cc0376c0027606
insertSoul <j.clarke213@gmail.com>
2022-12-23 10:01:12 +0000

    Create function to find the oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,29 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+   return array.reduce((oldest, currentPerson) => {
+       const oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+       const currentPersonAge = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
+       return oldestAge > currentPersonAge ? oldest : currentPerson ;
+});
 };
 
+/*
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
+*/
 // Do not edit below this line
 module.exports = findTheOldest;
